ab6e0a74f0477fe8da0b0549d634ec54ab9fb6ef
ed louise amor <ed.amor@xlog.com>
2020-12-23 04:32:06 +0800

    woop woop

--- src/main/java/org/edamor/numbers/controllers/Controller.java
+++ src/main/java/org/edamor/numbers/controllers/Controller.java
@@ -2,10 +2,8 @@ package org.edamor.numbers.controllers;
 
 
 import org.edamor.numbers.models.NumberResponseDto;
-import org.edamor.numbers.models.NumberTrivia;
 import org.edamor.numbers.services.NumberTriviaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
